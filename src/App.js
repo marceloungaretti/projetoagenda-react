c35b08924b2c1667378b0aad3af3da9ebbe2f1a4
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2021-07-29 13:42:19 -0300

    almost done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Main from './components/Main';
+
 import './App.css';
 
 export default function App() {
-  return <h1>Hellasdadasdasdas!</h1>;
+  return <Main />;
 }
